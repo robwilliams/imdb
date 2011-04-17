e2678c0ad4914d8cb97e6d13c91603317758dc14
Robert Williams <rob@r-williams.com>
2011-04-17 10:38:34 +0100

    Initial Rails Skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imdb::Application.config.secret_token = '5516bd79cb071665c41062717a51f7734b366668ecee525cc4968c9f389e7b7390242b121112f4025ccb5e9a13b09323b108652946624c03a08f37d93271e1f5'
